aba8aa0f9b1cb2a7185861ab796f5990707c66f2
codewithirene567 <irenejinscott@gmail.com>
2020-09-22 22:14:55 -0400

    installed thunk, react-redux, and redux packages

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import gameReducer from './reducers'
-import playerReducer from './reducers'
+import gameReducer from './reducers/gameReducer'
+import playerReducer from './reducers/playerReducer'
 
 import './index.css';
 import App from './App';
