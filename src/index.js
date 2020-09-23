cf5ff5b3a63464aae73262071e0944e6a358909c
codewithirene567 <irenejinscott@gmail.com>
2020-09-22 22:01:59 -0400

    made imports in index.js for needed parts including middleware for my project

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers'
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
