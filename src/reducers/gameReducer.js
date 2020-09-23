ac9e12de23a6d8038eeaa5cf08f006e99882125d
codewithirene567 <irenejinscott@gmail.com>
2020-09-22 22:32:41 -0400

    made the base functions of the reducers in my code

---
+++ src/reducers/gameReducer.js
@@ -0,0 +1,21 @@
+function gameReducer(state = { game: []}, action) {
+    switch (action.type) {
+   
+    //   case '':
+    //     return {
+    //       ...state,
+    //       game: [...state.game],
+    //       requesting: true
+    //     }
+   
+    //   case 'ADD_game':
+    //     return {
+    //       ...state,
+    //       game: action.game,
+    //       requesting: false
+    //     }
+   
+      default:
+        return state;
+    }
+  };
---
+++ src/reducers/playerReducer.js
@@ -0,0 +1,21 @@
+function playerReducer(state = { player: []}, action) {
+    switch (action.type) {
+   
+    //   case '':
+    //     return {
+    //       ...state,
+    //       player: [...state.player],
+    //       requesting: true
+    //     }
+   
+    //   case 'ADD_player':
+    //     return {
+    //       ...state,
+    //       player: action.player,
+    //       requesting: false
+    //     }
+   
+      default:
+        return state;
+    }
+  };
