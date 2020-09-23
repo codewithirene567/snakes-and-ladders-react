8953ff4c3779cd4e8e014dab4a6dc268f6b39054
codewithirene567 <irenejinscott@gmail.com>
2020-09-23 13:15:59 -0400

    made some new component files including dice.js and square.js, added some reducers, modified component logic in board

--- src/components/board.js
+++ src/components/board.js
@@ -1,1 +1,19 @@
+//container component
 //divs and colums
+import React, { Component } from 'react';
+
+export default class Board extends Component {
+
+makeSquares = () => (
+    <div id="1"  style="background-color: rgb(255, 255, 255);"></div>
+    <div id="2"  style="background-color: rgb(255, 255, 255);"></div>
+)
+
+    render () {
+        return (
+            <div id="squares">
+              {this.makeSquares()}
+            </div>
+        )
+    }
+}
