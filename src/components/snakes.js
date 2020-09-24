9a27f023683f25e49900b13f02f204373cb36663
codewithirene567 <irenejinscott@gmail.com>
2020-09-23 22:26:26 -0400

    wrote basic snake component

--- src/components/snakes.js
+++ src/components/snakes.js
@@ -1,1 +1,10 @@
 //presentational component
+import React from 'react'
+
+export default function Snake(props) {
+    return (
+        <div>
+            <img src="https://img.favpng.com/22/0/23/snakes-and-ladders-clip-art-reptile-cartoon-png-favpng-AaDiEwRL30PXpc43c0KJMBwij.jpg"></img>
+        </div>
+    )
+}
