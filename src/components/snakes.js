66f3776bde04b7eaa1afcbf1893a911808eae16e
codewithirene567 <irenejinscott@gmail.com>
2020-09-23 23:07:42 -0400

    modified snake component and css positioning for the snakes, also gave them all ids

--- src/components/snakes.js
+++ src/components/snakes.js
@@ -4,7 +4,15 @@ import React from 'react'
 export default function Snake(props) {
     return (
         <div>
-            <img src="https://img.favpng.com/22/0/23/snakes-and-ladders-clip-art-reptile-cartoon-png-favpng-AaDiEwRL30PXpc43c0KJMBwij.jpg"></img>
+        <div id="snake-1">
+            <img width="150" height="300"src="https://www5.lunapic.com/do-not-link-here-use-hosting-instead/160091497965385583?2781362168"></img>
         </div>
+         <div id="snake-2">
+         <img width="150" height="300"src="https://www5.lunapic.com/do-not-link-here-use-hosting-instead/160091497965385583?2781362168"></img>
+     </div>
+      <div id="snake-3">
+      <img width="150" height="300"src="https://www5.lunapic.com/do-not-link-here-use-hosting-instead/160091497965385583?2781362168"></img>
+  </div>
+  </div>
     )
 }
--- src/containers/Game.js
+++ src/containers/Game.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Dice from '../components/dice'
 import Board from '../components/board'
 import Player from '../components/player'
+import Snake from '../components/snakes'
 
 const Game = () => {
     return (
       <div>
         <Dice/>
         <Player />
+        <Snake />
         <Board />
         <h1>This is the game board</h1>
       </div>
