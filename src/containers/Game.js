3d6c61078b16ad509ea2db80edcb7bc122e45f48
codewithirene567 <irenejinscott@gmail.com>
2020-09-26 22:39:22 -0400

    wrote code to show the winner on the game route

--- src/containers/Game.js
+++ src/containers/Game.js
@@ -4,11 +4,13 @@ import Board from '../components/board'
 //import Player from '../components/player'
 import Snake from '../components/snakes'
 import Ladder from '../components/ladders'
+import { connect } from 'react-redux';
 
-const Game = () => {
+const Game = (props) => {
+  
     return (
       <div>
-        <h1>Snakes and Ladders</h1>
+        <h1>Snakes and Ladders - Player {props.winner} is winning!</h1>
         <Snake />
         <Dice/>
         <Ladder />
@@ -18,4 +20,29 @@ const Game = () => {
       </div>
     )
   }
-   export default Game;
+  //pass in props of all players
+  //find the position
+  const mapStateToProps = (state) => {
+     //console.log(state.playerReducer)
+     let array=[]
+     let highestName = ''
+    for(let i=0; i < state.playerReducer.allplayers.length; i++){
+
+      let position =  state.playerReducer.allplayers[i].currentPostion
+      
+      
+        array.push(position)
+       
+      }
+      let maximum = Math.max(...array) //4
+
+    for(let i=0; i < state.playerReducer.allplayers.length; i++){
+      if (maximum === state.playerReducer.allplayers[i].currentPostion){
+         highestName= state.playerReducer.allplayers[i].name
+      }
+      
+    }
+    return {winner:highestName}
+    
+}
+   export default connect(mapStateToProps, null)(Game);
