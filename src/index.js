5bf7c0f6cfd1e36cf609576c668cc30936c79f46
codewithirene567 <irenejinscott@gmail.com>
2020-10-28 10:33:25 -0400

    added cors middleware

--- src/index.js
+++ src/index.js
@@ -35,4 +35,19 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
-
+//------------------------------------
+let express = require('express')
+let cors = require('cors')
+let app = express()
+ 
+app.use(cors())
+app.use('/game', gameRoutes)
+app.use('/player', playerRoutes) 
+export default app;
+// app.get('/products/:id', function (req, res, next) {
+//   res.json({msg: 'This is CORS-enabled for all origins!'})
+// })
+ 
+app.listen(80, function () {
+  console.log('CORS-enabled web server listening on port 80')
+})
