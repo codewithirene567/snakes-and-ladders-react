f8c95da866ef71650b445a3139ed16b9c5fe31f8
codewithirene567 <irenejinscott@gmail.com>
2020-10-28 10:52:11 -0400

    added a proxy to my API requests

--- src/actions/gameActions.js
+++ src/actions/gameActions.js
@@ -20,7 +20,7 @@ export const saveGame = (game) =>{
         body: JSON.stringify(game)
         }
         console.log("c")
-         fetch('https://fast-journey-86003.herokuapp.com/game', configObj) 
+         fetch('https://cors-anywhere.herokuapp.com/https://fast-journey-86003.herokuapp.com/game', configObj) 
          
          .then(response => response.json())
          .then(data => {
@@ -70,7 +70,7 @@ export const savePlayersArray = (players, currentTimeStamp) => {
         },
         body: JSON.stringify(players)
         }
-        fetch('https://fast-journey-86003.herokuapp.com/player', configObj)    
+        fetch('https://cors-anywhere.herokuapp.com/https://fast-journey-86003.herokuapp.com/player', configObj)    
          .then(response => response.json())
          .then(data => {
             dispatch({type:'START_SAVE_PLAYER_REQUEST', payload: data})
@@ -83,7 +83,7 @@ export const savePlayersArray = (players, currentTimeStamp) => {
 export const getGame = (gameId) =>{
    
     return (dispatch) => {
-        fetch('https://fast-journey-86003.herokuapp.com/game/'+gameId)
+        fetch('https://cors-anywhere.herokuapp.com/https://fast-journey-86003.herokuapp.com/game/'+gameId)
         .then(response => response.json())
         .then(data => dispatch({type: 'GET_ONE_GAME', payload:data}))
       }
@@ -92,7 +92,7 @@ export const getGame = (gameId) =>{
 export const getAllGame = () =>{
    
     return (dispatch) => {
-        fetch('https://fast-journey-86003.herokuapp.com/game/')
+        fetch('https://cors-anywhere.herokuapp.com/https://fast-journey-86003.herokuapp.com/game/')
         .then(response => response.json())
         .then(data => dispatch({type: 'GET_THE_GAME', payload:data}))
       }
@@ -100,7 +100,7 @@ export const getAllGame = () =>{
 
 export const getPlayers = () =>{
     return (dispatch) => {
-        fetch(`https://fast-journey-86003.herokuapp.com/player`)
+        fetch(`https://cors-anywhere.herokuapp.com/https://fast-journey-86003.herokuapp.com/player`)
         .then(response => response.json())
         .then(data => dispatch({type: 'GET_THE_PLAYERS', payload:data}))
       }
@@ -122,7 +122,7 @@ export const getPlayers = () =>{
                 },
                 body: JSON.stringify(game)
                 }
-                 fetch('https://fast-journey-86003.herokuapp.com/game/'+id, configObj)    
+                 fetch('https://cors-anywhere.herokuapp.com/https://fast-journey-86003.herokuapp.com/game/'+id, configObj)    
                  .then(response => response.json())
                 //  .then(data => {
                 //     dispatch({type:'UPDATE_GAME', payload: data})
@@ -160,7 +160,7 @@ export const getPlayers = () =>{
                 },
                 body: JSON.stringify(players)
                 }
-                fetch('https://fast-journey-86003.herokuapp.com/player/'+players[0].id, configObj)    
+                fetch('https://cors-anywhere.herokuapp.com/https://fast-journey-86003.herokuapp.com/player/'+players[0].id, configObj)    
                  .then(response => response.json())
                  .then(data => {
                     dispatch({type:'UPDATE_PLAYERS', payload: data})
